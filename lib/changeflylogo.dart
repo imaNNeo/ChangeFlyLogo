ed40a219bef6110dfa30954c4a5d1f6ce307c868
imaNNeoFighT <iman.neofight@gmail.com>
2018-10-19 12:20:46 +0330

    added logo images

--- lib/changeflylogo.dart
+++ lib/changeflylogo.dart
@@ -9,9 +9,21 @@ class ChangeFlyLogo extends StatefulWidget {
 
 class _ChangeFlyLogoState extends State<ChangeFlyLogo> {
 
+  final double size = 200.0;
+  
   @override
   Widget build(BuildContext context) {
-    return Text("Nothing Yet :)");
+    return SizedBox(
+      width: size,
+      height: size,
+      child: Stack(
+        children: <Widget>[
+          Image.asset("assets/images/logo/changefly-cube-top.png"),
+          Image.asset("assets/images/logo/changefly-cube-left.png"),
+          Image.asset("assets/images/logo/changefly-cube-right.png"),
+        ],
+      ),
+    );
   }
 
 }
