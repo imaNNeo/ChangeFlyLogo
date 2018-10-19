d106a2f07ef05480f6845808e115b106368d493f
imaNNeoFighT <iman.neofight@gmail.com>
2018-10-20 01:43:18 +0330

    implemented logo animations and its pretty like to sample

--- lib/changeflylogo.dart
+++ lib/changeflylogo.dart
@@ -7,10 +7,11 @@ class ChangeFlyLogo extends StatefulWidget {
 }
 
 class _ChangeFlyLogoState extends State<ChangeFlyLogo>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   final double size = 200.0;
 
-  AnimationController controller;
+  AnimationController firstAnimationController;
+  AnimationController secondAnimationController;
 
   //Top section animations
   Animation topTranslateAnim;
@@ -27,100 +28,140 @@ class _ChangeFlyLogoState extends State<ChangeFlyLogo>
   Animation rightRotationXAnim;
   Animation rightRotationYAnim;
 
-  Curve myCurve = Cubic(0.45,-0.12,1.0,0.85);
+  //Whole Logo
+  Animation opacityAnimation;
+  Animation shakeAnimation;
+
+  Curve myCurve = Cubic(0.45, -0.12, 1.0, 0.85);
 
   @override
   void initState() {
     super.initState();
-    controller = new AnimationController(
+    firstAnimationController = new AnimationController(
         vsync: this, duration: new Duration(milliseconds: 600));
 
-    controller.addListener(() {
+    firstAnimationController.addListener(() {
       setState(() {});
     });
 
+    firstAnimationController.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        secondAnimationController.forward(from: 0.0);
+      }
+    });
+
+    secondAnimationController = new AnimationController(
+        vsync: this, duration: new Duration(milliseconds: 100));
+
+    secondAnimationController.addListener(() {
+      setState(() {});
+    });
+
+    secondAnimationController.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        secondAnimationController.reverse(from: 1.0);
+      }
+    });
+
     //Top section animations
-    topTranslateAnim = new Tween(
-        begin: const Offset(0.0, -200.0), end: const Offset(0.0, 0.0))
-        .animate(new CurvedAnimation(parent: controller, curve: myCurve));
+    topTranslateAnim =
+        new Tween(begin: const Offset(0.0, -280.0), end: const Offset(0.0, 0.0))
+            .animate(new CurvedAnimation(
+                parent: firstAnimationController, curve: myCurve));
 
     //Left section animations
-    leftScaleAnim = new Tween(begin: 3.0, end: 1.0)
-        .animate(new CurvedAnimation(parent: controller, curve: Curves.ease));
+    leftScaleAnim = new Tween(begin: 3.0, end: 1.0).animate(new CurvedAnimation(
+        parent: firstAnimationController, curve: Curves.ease));
 
-    leftTranslateAnim = new Tween(
-        begin: const Offset(-80.0, 80.0), end: Offset.zero)
-        .animate(
-        new CurvedAnimation(parent: controller, curve: myCurve));
+    leftTranslateAnim =
+        new Tween(begin: const Offset(-80.0, 80.0), end: Offset.zero).animate(
+            new CurvedAnimation(
+                parent: firstAnimationController, curve: myCurve));
 
-    leftRotationXAnim = new Tween(begin: 1.2, end: 0.0)
-        .animate(
-        new CurvedAnimation(parent: controller, curve: Curves.linear));
+    leftRotationXAnim = new Tween(begin: 1.2, end: 0.0).animate(
+        new CurvedAnimation(
+            parent: firstAnimationController, curve: Curves.linear));
 
-    leftRotationYAnim = new Tween(begin: 0.3, end: 0.0)
-        .animate(
-        new CurvedAnimation(parent: controller, curve: Curves.linear));
-
+    leftRotationYAnim = new Tween(begin: 0.3, end: 0.0).animate(
+        new CurvedAnimation(
+            parent: firstAnimationController, curve: Curves.linear));
 
     //Right section animations
-    rightScaleAnim = new Tween(begin: 3.0, end: 1.0)
-        .animate(new CurvedAnimation(parent: controller, curve: Curves.ease));
+    rightScaleAnim = new Tween(begin: 3.0, end: 1.0).animate(
+        new CurvedAnimation(
+            parent: firstAnimationController, curve: Curves.ease));
 
-    rightTranslateAnim = new Tween(
-        begin: const Offset(80.0, 80.0), end: Offset.zero)
-        .animate(
-        new CurvedAnimation(parent: controller, curve: myCurve));
+    rightTranslateAnim =
+        new Tween(begin: const Offset(80.0, 80.0), end: Offset.zero).animate(
+            new CurvedAnimation(
+                parent: firstAnimationController, curve: myCurve));
 
-    rightRotationXAnim = new Tween(begin: -1.2, end: 0.0)
-        .animate(
-        new CurvedAnimation(parent: controller, curve: Curves.linear));
+    rightRotationXAnim = new Tween(begin: -1.2, end: 0.0).animate(
+        new CurvedAnimation(
+            parent: firstAnimationController, curve: Curves.linear));
 
-    rightRotationYAnim = new Tween(begin: 0.3, end: 0.0)
-        .animate(
-        new CurvedAnimation(parent: controller, curve: Curves.linear));
+    rightRotationYAnim = new Tween(begin: 0.3, end: 0.0).animate(
+        new CurvedAnimation(
+            parent: firstAnimationController, curve: Curves.linear));
 
+    //Whole View
+    opacityAnimation = new Tween(begin: 0.0, end: 1.0).animate(
+        new CurvedAnimation(
+            parent: firstAnimationController, curve: Curves.linear));
 
-    controller.forward(from: 0.0);
+    shakeAnimation = new Tween(begin: 1.0, end: 1.05).animate(
+        new CurvedAnimation(
+            parent: secondAnimationController, curve: Curves.fastOutSlowIn));
+
+    firstAnimationController.forward(from: 0.0);
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        controller.forward(from: 0.0);
+        firstAnimationController.forward(from: 0.0);
       },
       child: SizedBox(
         width: size,
         height: size,
-        child: Stack(
-          children: <Widget>[
-            Transform(
-              transform: Matrix4.identity()
-                ..translate(
-                    topTranslateAnim.value.dx, topTranslateAnim.value.dy),
-              child: Image.asset("assets/images/logo/changefly-cube-top.png"),
+        child: Transform.scale(
+          scale: shakeAnimation.value,
+          child: Opacity(
+            opacity: opacityAnimation.value,
+            child: Stack(
+              children: <Widget>[
+                Transform(
+                  transform: Matrix4.identity()
+                    ..translate(
+                        topTranslateAnim.value.dx, topTranslateAnim.value.dy),
+                  child: Image.asset("assets/images/logo/changefly-cube-top.png"),
+                ),
+                Transform(
+                  alignment: Alignment(-0.75, 0.1),
+                  transform: Matrix4.identity()
+                    ..translate(
+                        leftTranslateAnim.value.dx, leftTranslateAnim.value.dy)
+                    ..scale(leftScaleAnim.value)
+                    ..rotateX(leftRotationXAnim.value)
+                    ..rotateY(leftRotationYAnim.value),
+                  child:
+                      Image.asset("assets/images/logo/changefly-cube-left.png"),
+                ),
+                Transform(
+                  alignment: Alignment(0.75, 0.1),
+                  transform: Matrix4.identity()
+                    ..translate(
+                        rightTranslateAnim.value.dx, rightTranslateAnim.value.dx)
+                    ..scale(rightScaleAnim.value)
+                    ..rotateX(rightRotationXAnim.value)
+                    ..rotateY(rightRotationYAnim.value),
+                  child:
+                      Image.asset("assets/images/logo/changefly-cube-right.png"),
+                ),
+              ],
             ),
-            Transform(
-              alignment: Alignment(-0.75, 0.1),
-              transform: Matrix4.identity()
-                ..translate(
-                    leftTranslateAnim.value.dx, leftTranslateAnim.value.dy)
-                ..scale(leftScaleAnim.value)
-                ..rotateX(leftRotationXAnim.value)
-                ..rotateY(leftRotationYAnim.value),
-              child: Image.asset("assets/images/logo/changefly-cube-left.png"),
-            ),
-            Transform(
-              alignment: Alignment(0.75, 0.1),
-              transform: Matrix4.identity()
-                ..translate(
-                    rightTranslateAnim.value.dx, rightTranslateAnim.value.dx)
-                ..scale(rightScaleAnim.value)
-                ..rotateX(rightRotationXAnim.value)
-                ..rotateY(rightRotationYAnim.value),
-              child: Image.asset("assets/images/logo/changefly-cube-right.png"),
-            ),
-          ],
+          ),
         ),
       ),
     );
@@ -129,12 +170,14 @@ class _ChangeFlyLogoState extends State<ChangeFlyLogo>
   @override
   void dispose() {
     super.dispose();
-    controller.dispose();
+    firstAnimationController.dispose();
+    secondAnimationController.dispose();
   }
 }
 
-class MyCurve extends Curve{
+class MyCurve extends Curve {
   const MyCurve([this.period = 0.4]);
+
   final double period;
 
   @override
@@ -142,11 +185,19 @@ class MyCurve extends Curve{
     assert(t >= 0.0 && t <= 1.0);
     final double s = 0.1;
     t = t - 1.0;
-    return -math.pow(2.0, 10.0 * t) * math.sin((t - s) * (math.pi * 2.0) / period);
+    return -math.pow(2.0, 10.0 * t) *
+        math.sin((t - s) * (math.pi * 2.0) / period);
   }
 
   @override
   String toString() {
     return '$runtimeType($period)';
+  }
+}
+
+class ShakeCurve extends Curve {
+  @override
+  double transform(double t) {
+    return math.sin(t * math.pi * 2);
   }
 }
