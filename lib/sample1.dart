50f7c0a17150283e056880d157e33bb076d087f3
imaNNeoFighT <iman.neofight@gmail.com>
2018-10-20 13:23:05 +0330

    added scaffold on sample1

--- lib/sample1.dart
+++ lib/sample1.dart
@@ -3,26 +3,31 @@ import 'package:flutter/material.dart';
 class Sample1 extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _Sample1State();
-
 }
 
 class _Sample1State extends State<Sample1> {
-
   final double size = 200.0;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: size,
-      height: size,
-      child: Stack(
-        children: <Widget>[
-          Image.asset("assets/images/logo/changefly-cube-top.png"),
-          Image.asset("assets/images/logo/changefly-cube-left.png"),
-          Image.asset("assets/images/logo/changefly-cube-right.png"),
-        ],
-      )
+    return Scaffold(
+      appBar: AppBar(title: Text("Sample 1"),),
+      body: Center(
+        child: SizedBox(
+            width: size,
+            height: size,
+            child: Stack(
+              children: <Widget>[
+                Transform(
+                    transform: Matrix4.identity()
+                    ..translate(0.0, -10.0),
+                    child:
+                        Image.asset("assets/images/logo/changefly-cube-top.png")),
+                Image.asset("assets/images/logo/changefly-cube-left.png"),
+                Image.asset("assets/images/logo/changefly-cube-right.png"),
+              ],
+            )),
+      ),
     );
   }
-
 }
